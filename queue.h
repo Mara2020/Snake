ec2108b9c577cd8f45ce488c85ba9c0a4ce14525
Bianca Angotti <bangotti@ualberta.ca>
2016-01-11 15:22:55 -0700

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,57 @@
+
+#ifndef _queue_h_
+#define _queue_h_
+
+/*
+  struct for implementing a queue using
+  a wrap-around array
+*/
+typedef struct {
+  int* x;    // the buffer x 
+  int* y;    // the buffer y
+  int length;  // # of items currently in queue
+  int start;   // start index of the front
+  int buf_len; // # of items that can be stored
+} q_array_t;
+
+
+//Our implementaiton will be "modular", we can change
+//the internal representation and implementations with
+//impact on how the user uses the code
+typedef q_array_t* queue;
+
+/*
+  Constructor: create an instance of the
+  queue and return a pointer to that instance.
+*/
+queue q_create(int max_size);
+
+/*
+  Destructor: free up the space used by
+  the queue.
+*/
+void q_destroy(queue q);
+
+/*
+  return the length of the queue
+*/
+int q_length(queue q);
+
+/*
+  add val to the end of the queue
+*/
+void q_add(queue q, int valx, int valy);
+
+/*
+  return the item at the front of the queue
+*/
+int q_frontx(queue q);
+int q_fronty(queue q);
+
+/*
+  remove the item at the front of the queue
+*/
+void q_remove(queue q);
+
+
+#endif
